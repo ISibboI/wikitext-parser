7cb8a069d6fa916e01ba56d4876c78dbea2e8fab
Sebastian Schmidt <isibboi@gmail.com>
2022-05-27 12:29:56 +0300

    Create stub.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+#![warn(missing_docs)]
+
+use error::Result;
+
+mod error;
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+mod tests;
+
+/// The root of a wikitext document.
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Wikitext;
+
+/// Parse textual wikitext into a semantic representation.
+pub fn parse_wikitext(wikitext: &str) -> Result<Wikitext> {
+    todo!()
 }
